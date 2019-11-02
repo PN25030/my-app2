12ac02bdd9e20f8767629a8a9f95a6769722b1c3
Piyush Namra <namrapiyush@gmail.com>
2019-11-02 21:05:13 +0000

    Test Change

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -28,7 +28,7 @@ export default function SearchAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
-            Contacts
+            Rohini
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
